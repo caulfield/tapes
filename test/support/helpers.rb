b315c4769453a0906a0f880b2578ff8359b33e78
Sergey Kuchmistov <sergey.kuchmistov@gmail.com>
2014-08-21 15:44:32 +0300

    delete unused method

--- test/support/helpers.rb
+++ test/support/helpers.rb
@@ -1,13 +1,4 @@
 module Helpers
-  def store_translations(locale, translations, &block)
-    begin
-      I18n.backend.store_translations locale, translations
-      yield
-    ensure
-      I18n.reload!
-    end
-  end
-
   def concat_form_for(object, options={}, &block)
     options.merge!(tapes: true)
     concat form_for object, options, &block
